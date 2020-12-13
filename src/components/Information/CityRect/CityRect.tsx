3dc8854d7f7abd94944a179f1b470ccd59b26f51
Christian <christianlindeneg@gmail.com>
2020-12-13 06:14:27 +0100

    handle case where no city is passed

--- src/components/Information/CityRect/CityRect.tsx
+++ src/components/Information/CityRect/CityRect.tsx
@@ -1,27 +1,33 @@
 import ICityRectProps from "./CityRect.props";
 import CityResources from './CityResources/CityResources';
 import styles from './CityRect.module.css';
+import { CityResource } from "nardis-game";
 
 
 const rect = (props: ICityRectProps) => {
-    let jsx: JSX.Element = <div>No City Found</div>;
+    let ch: number = 0;
+    let name: string = 'NO DESTINATION COULD BE FOUND';
+    let resources: CityResource[] = [];
+    let sizeJSX: JSX.Element | null = null;
+
     if (props.city) {
-        const name: string = props.city.name;
-        const resources = props.from ? props.city.getSupply() : props.city.getDemand();
-        const size: JSX.Element | null = props.showSize ? <span> | SIZE {props.city.getSize()}</span> : null;
-        jsx = (
-            <div className={styles.CityRect}>
-                <h4 
-                    className={styles.CityName}
-                    style={{color: props.cityNameColor}}
-                >
-                    {name.toUpperCase()} {props.from ? 'SUPPLY' : 'DEMAND'} {size}
-                </h4>
-                <CityResources resources={resources} />
-            </div>
-        );
+        ch = 1;
+        name = props.city.name;
+        resources = props.from ? props.city.getSupply() : props.city.getDemand();
+        sizeJSX = props.showSize ? <span> | SIZE {props.city.getSize()}</span> : null;
     }
-    return jsx;
+
+    return (
+        <div className={styles.CityRect}>
+            <h4 
+                className={styles.CityName}
+                style={{color: props.cityNameColor}}
+            >
+                {name.toUpperCase()} {ch ? (props.from ? 'SUPPLY' : 'DEMAND') : null} {sizeJSX}
+            </h4>
+            <CityResources resources={resources} />
+        </div>
+    );;
 }
 
 export default rect;
