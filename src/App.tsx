5571ab026989f7dc378b725ef846d8e8c3777ccb
Christian <christianlindeneg@gmail.com>
2021-01-08 01:13:41 +0100

    (finally) add finance route

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import Layout from './containers/Layout/Layout';
 import Management from './containers/Management/Management';
+import Finance from './containers/Finance/Finance';
 
 const app = (): JSX.Element => (
 	<div>
 		<Layout>
 			<Switch>
 				<Route path="/management" component={Management} />	
-
+				<Route path="/finance" component={Finance} />	
 			</Switch>
 		</Layout>
 	</div>
