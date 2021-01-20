50235c05b3b8b32dfaea593d475ec7efdf28ff0d
Christian <christianlindeneg@gmail.com>
2021-01-20 07:50:04 +0100

    show the appropiate component to manage

--- src/containers/Management/Management.tsx
+++ src/containers/Management/Management.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import NewRoute from './NewRoute/NewRoute';
 import BuildQueue from './BuildQueue/BuildQueue';
@@ -7,22 +7,45 @@ import NavigationEntries from '../../components/Navigation/NavigationEntries/Nav
 import Styles from './Management.module.css';
 import { managementNavigationBarEntries } from '../../common/constants';
 import { Functional, Props } from '../../common/props';
+import * as Nardis from 'nardis-game';
+import NardisState from '../../common/state';
+import { connect } from 'react-redux';
 
+
+interface ManagementMappedProps {
+    routes: Nardis.Route[],
+    queue: Nardis.QueuedRouteItem[]
+}
+
+interface ManagementProps extends Props, ManagementMappedProps {}
+
+
+const mapStateToProps = (state: NardisState): ManagementMappedProps => ({
+    routes: state.routes,
+    queue: state.queue
+});
 
 
 /**
  * Container with components to create/manage routes
  */
-const management: Functional<Props> = (props: Props): JSX.Element => (
+const management: Functional<ManagementProps> = (
+    props: ManagementProps
+): JSX.Element => (
     <div className={Styles.Management}>
         <NavigationEntries style={{flexDirection: 'row', textAlign: 'center'}} {...managementNavigationBarEntries} />
         <Switch>
             <Route exact path="/management/new-route" component={NewRoute}/>
             <Route exact path="/management/build-queue" component={BuildQueue}/>
             <Route exact path="/management/manage-route" component={ManageRoute}/>
+            <Redirect 
+                to={"/management" + (
+                    props.queue.length > 0 ? '/build-queue' : (props.routes.length > 0 ? '/manage-route' : '/new-route')
+                )} 
+            />
         </Switch>
     </div>
 );
 
 
-export default management;
+export default connect(mapStateToProps)(management);
