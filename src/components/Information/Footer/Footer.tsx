2e2bd6c05d3bd8462a64dd6ead41d1478b811ce2
Christian <christianlindeneg@gmail.com>
2021-02-26 18:35:36 +0100

    implement footer

---
+++ src/components/Information/Footer/Footer.tsx
@@ -0,0 +1,47 @@
+import { Component, Fragment } from 'react';
+import Styles from './Footer.module.css';
+
+
+interface FooterState {
+    resetInitiated: boolean
+};
+
+interface FooterProps {
+    onResetGame: () => void;
+};
+
+
+/**
+ * Footer element with options to visit source code or reset an active game.
+ */
+class Footer extends Component<FooterProps, FooterState> {
+
+    state: FooterState = {
+        resetInitiated: false
+    };
+
+    onToggleReset = (): void => {
+        this.setState({
+            resetInitiated: !this.state.resetInitiated
+        });
+    }
+
+    render() {
+        return (
+            <footer className={Styles.Footer}>
+                <span>christian lindeneg @ 2021</span> -
+                <span className={Styles.Clickable}><a href='https://github.com/lindeneg/nardis-ui' target='_blank' rel='noreferrer' >source</a></span> -
+                {this.state.resetInitiated ? (
+                    <Fragment>
+                        <span>are you sure?</span> -
+                        <span className={Styles.Clickable} onClick={this.props.onResetGame}>yes</span> -
+                        <span className={Styles.Clickable} onClick={this.onToggleReset}>no</span>
+                    </Fragment>
+                ) : <span className={Styles.Clickable} onClick={this.onToggleReset}>reset game</span>}
+            </footer>
+        );
+    }
+}
+
+
+export default Footer;
